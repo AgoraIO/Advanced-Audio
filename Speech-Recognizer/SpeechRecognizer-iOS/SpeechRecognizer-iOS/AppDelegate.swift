366478b7cdb0484537e6e68ddcd50434947482bf
GongYuhua <gongyuhua@gmail.com>
2019-07-08 11:41:43 +0800

    speech recognizer

---
+++ Speech-Recognizer/SpeechRecognizer-iOS/SpeechRecognizer-iOS/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  SpeechRecognizer-iOS
+//
+//  Created by GongYuhua on 2019/7/8.
+//  Copyright © 2019 Agora. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
