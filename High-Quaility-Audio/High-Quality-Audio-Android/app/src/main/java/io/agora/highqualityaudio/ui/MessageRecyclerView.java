cb86e87a6dae059dade75d58ddf3bc65e988999e
brianxcli <neteasy1210@gmail.com>
2019-08-15 17:14:31 +0800

    add message line separators

--- High-Quaility-Audio/High-Quality-Audio-Android/app/src/main/java/io/agora/highqualityaudio/ui/MessageRecyclerView.java
+++ High-Quaility-Audio/High-Quality-Audio-Android/app/src/main/java/io/agora/highqualityaudio/ui/MessageRecyclerView.java
@@ -1,18 +1,22 @@
 package io.agora.highqualityaudio.ui;
 
 import android.content.Context;
+import android.graphics.Rect;
 import android.util.AttributeSet;
+import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import io.agora.highqualityaudio.R;
 import io.agora.highqualityaudio.adapters.MessageRecyclerAdapter;
 import io.agora.highqualityaudio.data.UserAccountManager;
 
 public class MessageRecyclerView extends RecyclerView {
     private MessageRecyclerAdapter mAdapter;
+    private int mMessageMargin;
 
     public MessageRecyclerView(@NonNull Context context) {
         super(context);
@@ -35,6 +39,20 @@ public class MessageRecyclerView extends RecyclerView {
         LinearLayoutManager manager = new LinearLayoutManager(context);
         manager.setOrientation(VERTICAL);
         setLayoutManager(manager);
+
+        mMessageMargin = context.getResources().
+                getDimensionPixelSize(R.dimen.room_message_padding) / 2;
+        addItemDecoration(new ItemDecoration() {
+            @Override
+            public void getItemOffsets(@NonNull Rect outRect, @NonNull View view,
+                                       @NonNull RecyclerView parent, @NonNull State state) {
+                int pos = parent.getChildAdapterPosition(view);
+                outRect.top = mMessageMargin;
+                outRect.bottom = mMessageMargin;
+                if (pos == 0) outRect.top = 0;
+                else if (pos == parent.getChildCount() - 1) outRect.bottom = 0;
+            }
+        });
     }
 
     public void setUser(UserAccountManager.UserAccount account) {
