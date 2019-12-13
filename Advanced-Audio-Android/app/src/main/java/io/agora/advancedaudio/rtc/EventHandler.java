25c70d79d18056cc5517584c6ba543a9913ca631
brianxcli <neteasy1210@gmail.com>
2019-12-13 17:25:17 +0800

    proj init

---
+++ Advanced-Audio-Android/app/src/main/java/io/agora/advancedaudio/rtc/EventHandler.java
@@ -0,0 +1,23 @@
+package io.agora.advancedaudio.rtc;
+
+import io.agora.rtc.IRtcEngineEventHandler;
+
+public interface EventHandler {
+    void onLeaveChannel(IRtcEngineEventHandler.RtcStats stats);
+
+    void onJoinChannelSuccess(String channel, int uid, int elapsed);
+
+    void onUserOffline(int uid, int reason);
+
+    void onUserJoined(int uid, int elapsed);
+
+    void onLastmileQuality(int quality);
+
+    void onLastmileProbeResult(IRtcEngineEventHandler.LastmileProbeResult result);
+
+    void onRtcStats(IRtcEngineEventHandler.RtcStats stats);
+
+    void onNetworkQuality(int uid, int txQuality, int rxQuality);
+
+    void onRemoteAudioStats(IRtcEngineEventHandler.RemoteAudioStats stats);
+}
